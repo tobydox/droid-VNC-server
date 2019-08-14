e5e70e0ea74e5a88f066dee7fa38eea7f4b5fe8d
Tobias Junghans <tobydox@veyon.io>
2019-08-14 18:43:48 +0200

    screenMethods: adb: add missing header

--- jni/vnc/screenMethods/adb.c
+++ jni/vnc/screenMethods/adb.c
@@ -30,6 +30,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <strings.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
